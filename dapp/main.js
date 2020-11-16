c3a464307e6f7006b26d9ce394051d03f17483c9
Sam Liu <sam@smscentral.com.au>
2020-11-16 11:09:46 +1100

    added support for account switching in metamask

--- dapp/main.js
+++ dapp/main.js
@@ -1,28 +1,34 @@
 var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
-let refreshBalance = function(accounts){
-    web3.eth.getBalance(contractInstance._address).then(function(res){
+
+window.ethereum.on('accountsChanged', function (accounts) {
+    hideAlerts();
+    refreshAccount(accounts);
+    refreshBalance(accounts);
+})
+
+let refreshBalance = function(accounts) {
+    contractInstance.methods.poolBalance().call().then(function(res){
         $('#balance_output').text(web3.utils.fromWei(res, "ether"));
     });
     web3.eth.getBalance(accounts[0]).then(function(res){
         $('#wallet_address_output').text(accounts[0]);
         $('#wallet_balance_output').text(web3.utils.fromWei(res, "ether"));
     });
+    contractInstance.methods.getUser().call().then(function(res){
+        $('#winning_balance_output').text(web3.utils.fromWei(res[3], "ether"));
+        if (res[3] > 0) {
+            $('#withdraw_button').removeClass('d-none');
+        } else {
+            $('#withdraw_button').addClass('d-none');
+        }
+    });
 }
 
-let hideAlerts = function(){
-    $('#win_alert').hide();
-    $('#lose_alert').hide();
-    $('#error_alert').hide();
-}
-
-$(document).ready(function() {
-    hideAlerts();
-    window.ethereum.enable().then(function(accounts){
-        contractInstance = new web3.eth.Contract(abi, "0x8EED7DA3385dD1eB9e02C4f41881349255d51FA2", {from: accounts[0]});
+let refreshAccount = function(accounts) {
+    contractInstance = new web3.eth.Contract(abi, "0x1CE5879b90627d2A97c4B76A1fF31E8eA31B9183", {from: accounts[0]});
         console.log(contractInstance);
         refreshBalance(accounts);
-
 
         contractInstance.events.userWin()
         .on('data', (event) => {
@@ -41,13 +47,26 @@ $(document).ready(function() {
             refreshBalance(accounts);
         })
         .on('error', console.error);
+}
+
+
+let hideAlerts = function(){
+    $('#win_alert').hide();
+    $('#lose_alert').hide();
+    $('#error_alert').hide();
+}
+
+$(document).ready(function() {
+    hideAlerts();
+    window.ethereum.enable().then(function(accounts){
+        refreshAccount(accounts);
     });
     
     $("#place_bet_button").click(function(){
         hideAlerts();
         var wager = web3.utils.toWei($('#amount_input').val(), "ether");
         if(wager > 0) {
-            contractInstance.methods.bet()
+            contractInstance.methods.placeBet()
             .send({value: wager})
             .on("receipt", function(receipt){
                 console.log(receipt);
